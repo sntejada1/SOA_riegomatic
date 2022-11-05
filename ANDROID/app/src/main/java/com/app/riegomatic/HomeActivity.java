bdbf0776ebb85f9c3f75e97006c5f5a98859a8ea
Agustin Dilascio <63321073+AgustinDilascio@users.noreply.github.com>
2022-11-04 21:39:56 -0300

    primera conexion

--- ANDROID/app/src/main/java/com/app/riegomatic/HomeActivity.java
+++ ANDROID/app/src/main/java/com/app/riegomatic/HomeActivity.java
@@ -1,18 +1,59 @@
 package com.app.riegomatic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.util.Log;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.annotation.SuppressLint;
+import android.os.Handler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class HomeActivity extends AppCompatActivity implements Contract.ViewMVP {
 
     private TextView textStatus;
     private Contract.PresenterMVP presenter;
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder recDataString = new StringBuilder();
+    private ConnectedThread mConnectedThread;
+    private static final String TAG = "HomeActivity";
 
+
+    // SPP UUID service - this should work for most devices
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // String for MAC address
+    private static String address = null;
+
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,19 +63,227 @@ public class HomeActivity extends AppCompatActivity implements Contract.ViewMVP 
         Button btn_back = findViewById(R.id.back);
         Button btn_watering = findViewById(R.id.watering);
 
+
         btn_back.setOnClickListener(btnListener);
         btn_watering.setOnClickListener(btnListener);
-        
-        presenter = new Presenter(this);
 
-        // btn_back.setOnClickListener(new View.OnClickListener() {
-        //     @Override
-        //     public void onClick(View view) {
-        //         Intent intent2 = new Intent(view.getContext(), MainActivity.class);
-        //         startActivityForResult(intent2,0);
+        presenter = new Presenter(this);
+        // bluetooth
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                 Log.d(TAG, "...AQUIIIIIII0............................................................." + msg.toString());
+                if (msg.what == handlerState) {          //if message is what we want
+                    String readMessage = (String) msg.obj;                                                                // msg.arg1 = bytes from connect thread
+                 Log.d(TAG, "...AQUIIIIIII1............................................................." + readMessage.toString());
+                    recDataString.append(readMessage);              //keep appending to string until ~
+                    int endOfLineIndex = recDataString.indexOf("~");                    // determine the end-of-line
+                    if (endOfLineIndex > 0) {    
+                        Log.d(TAG, "...AQUIIIIIII2............................................................." );                                       // make sure there data before ~
+                        String dataInPrint = recDataString.substring(0, endOfLineIndex);    // extract string
+                        //txtString.setText("Datos recibidos = " + dataInPrint);
+                        int dataLength = dataInPrint.length();       //get length of data received
+                        //txtStringLength.setText("Tamaño del String = " + String.valueOf(dataLength));
+                        if (recDataString.charAt(0) == '#')        //if it starts with # we know it is what we are looking for
+                        {
+                            Log.d(TAG, "...AQUIIIIIII3............................................................." );
+                            String sensor0 = recDataString.substring(1, 5);             //get sensor value from string between indices 1-5
+                            String sensor1 = recDataString.substring(6, 10);            //same again...
+                            String sensor2 = recDataString.substring(11, 15);
+                            String sensor3 = recDataString.substring(16, 20);
+
+                            if (sensor0.equals("1.00"))
+                                textStatus.setText("Encendido"); //update the textviews with sensor values
+                            else
+                                textStatus.setText("Apagado"); //update the textviews with sensor values
+                            // sensorView1.setText(sensor1);
+                            // sensorView2.setText(sensor2);
+                            // sensorView3.setText(sensor3);
+                            //sensorView3.setText(" Sensor 3 Voltage = " + sensor3 + "V");
+                        }
+                        recDataString.delete(0, recDataString.length());      //clear all string data
+                        // strIncom =" ";
+                        dataInPrint = " ";
+                    }
+                }
+            }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();       // get Bluetooth adapter
+        checkBTState();
+
+        // Set up onClick listeners for buttons to send 1 or 0 to turn on/off LED
+        // btnOff.setOnClickListener(new OnClickListener() {
+        //     public void onClick(View v) {
+        //         mConnectedThread.write("2");    // Send "0" via Bluetooth
+        //         Toast.makeText(getBaseContext(), "Apagar el LED", Toast.LENGTH_SHORT).show();
         //     }
         // });
+
+        //  btnOn.setOnClickListener(new OnClickListener() {
+        //     public void onClick(View v) {
+        //         mConnectedThread.write("1");    // Send "1" via Bluetooth
+        //         Toast.makeText(getBaseContext(), "Encender el LED", Toast.LENGTH_SHORT).show();
+        //     }
+        // });
+
+
     }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        }
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+         Log.d(TAG, "...onResumeeeeeeeeeee.............................................................");
+
+        //Get MAC address from DeviceListActivity via intent
+        Intent intent = getIntent();
+
+        //Get the MAC address from the HomeActivity via EXTRA
+        // address = intent.getStringExtra(HomeActivity.EXTRA_DEVICE_ADDRESS);
+        // address = intent.getStringExtra("98:D3:61:F9:39:A5");
+        address = "98:D3:61:F9:39:A5";
+
+        //create device and set the MAC address
+        //Log.i("ramiro", "adress : " + address);
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+        // Establish the Bluetooth socket connection.
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                // return;
+            }
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                //insert code to deal with this
+            }
+        }
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+
+        //I send a character when resuming.beginning transmission to check device is connected
+        //If it is not an exception will be thrown in the write method and finish() will be called
+        mConnectedThread.write("x");
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            //Don't leave Bluetooth sockets open when leaving activity
+            btSocket.close();
+        } catch (IOException e2) {
+            //insert code to deal with this
+        }
+    }
+
+    //Checks that the Android device Bluetooth is available and prompts to be turned on if off
+    private void checkBTState() {
+
+        if (btAdapter == null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    // return;
+                }
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    //create new class for connect thread
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            // Keep looping to listen for received messages
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);         //read bytes from input buffer
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Send the obtained bytes to the UI Activity via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+                finish();
+
+            }
+        }
+    }
+
+    // fin bluetooh
 
     @SuppressLint("Range")
     @Override
@@ -50,6 +299,7 @@ public class HomeActivity extends AppCompatActivity implements Contract.ViewMVP 
             switch(view.getId()) {
                 case R.id.watering:
                     presenter.onButtonClick();
+                    mConnectedThread.write("1");
                     break;
                 default:
                     throw new IllegalStateException("Unexpexted value" + view.getId());
--- ANDROID/app/src/main/java/com/app/riegomatic/HomeModel.java
+++ ANDROID/app/src/main/java/com/app/riegomatic/HomeModel.java
@@ -1,8 +1,11 @@
 package com.app.riegomatic;
 
 public class HomeModel implements Contract.ModelMVP{
+    
+
     @Override
     public void sendMessage(OnSendToPresenter presenter) {
         presenter.onFinished("Regando");
     }
+
 }
